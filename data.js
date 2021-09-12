8a437fcf643665f3bf085e9d6e21e8d0c17163a3
GHSRobert Ciang <Robertus0617@gmail.com>
2021-09-13 02:06:50 +0800

    data 外放 ✅

---
+++ data.js
@@ -0,0 +1,37 @@
+data = [
+  {
+    "EP": "9",
+    "title": "",
+    "sections": [
+      {
+        "t": "1:50",
+        "rows": [
+          [
+            "村人",
+            "番仔杰，老規矩，𫣆兜這客人庄，有人十六歲吔，愛有成年禮！",
+            "Fánè Kied, lò gúigì, éndéu ìa Hàgngǐn zóng, íu ngǐn siiblìug se ě, oi íu sǐinngǐanlí!",
+            "fanˋ er kiedˋ, loˊ guiˋ giˊ , en deuˋ liaˊ hag ngin zongˋ, rhiuˋ ngin shibˋ liug soiˇ leˋ , oiˇ rhiuˋ shin ngien liˋ!"
+          ]
+        ]
+      },
+      {
+        "t": "6:00",
+        "rows": []
+      },
+      {
+        "t": "10:15",
+        "rows": []
+      },
+      {
+        "t": "33:00",
+        "rows": []
+      }
+    ]
+  },
+  {
+    "EP": "10",
+    "title": "ljayar 瘟疫",
+    "sections": [],
+    "misc": "（像完全無人講着客話 XD）"
+  }
+]
