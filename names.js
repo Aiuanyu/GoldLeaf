9166977aa0b37d4e4e7487ca4635c8bf81f5cebf
GHSRobert Ciang <Robertus0617@gmail.com>
2021-09-14 22:33:02 +0800

    names.js 外放

---
+++ names.js
@@ -0,0 +1,21 @@
+names ={
+  "村人": ["Cúnngǐn","cunˋ ngin"],
+  "杰": ["Kied","kiedˋ"],
+  "水仔": ["Sùiè","shuiˊ er"],
+  "林老實": ["Lǐm Lòsiid","lim loˊ shidˋ"],
+  "阿九": ["Á-Gìu","a+ giuˊ"],
+  "林": ["Lǐm","lim"],
+  "九": ["Gìu","giuˊ"],
+  "水": ["Sùi","shuiˊ"],
+  "水2": ["Tsuí", "Tsuí"],
+  "船員": ["Sǒnǐan","shon rhan"],
+  "蝶": ["Tiab","tiabˋ"],
+  "瑪祖卡": ["Macuka","Macuka"],
+  "丙": ["Bíang","biangˋ"],
+  "姆": ["Mé","meˋ"],
+  "Tok": ["Tok","Tok"],
+  "必": ["Pickering", "Pickering"],
+  "李": ["LeGendre", "LeGendre"],
+  "軍官5": ["Jūnguān"],
+  "劉5": ["Líu"]
+}
