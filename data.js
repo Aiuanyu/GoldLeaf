6f382cbeb460cfb09aacf65f201878e18361c547
GHSRobert Ciang <Robertus0617@gmail.com>
2021-11-17 00:19:16 +0800

    v1.1.2 多做一段 + PDF 上線
    - 日文字型
    - CSS 開始用 variable
    - 按鈕顏色主題化

--- data.js
+++ data.js
@@ -21,7 +21,8 @@ data = [
             "希瓦",
             "沒錯。謝謝。",
             "Yeah, I think so. <span class='noted classic' style='font-size: 1.5em;'><ruby><rb style=' font-family: Mangal, sans-serif; font-weight: 500;'>धन्यवाद</rb><rp>(</rp><rt>Dhanyavaad</rt><rp>)</rp></ruby></span>.<div class='sidenote'>特別做這段英語對白，就是為了分享這個特別的字。它是 Hindi 語和尼泊爾語的「謝謝」，據說兩種語言很相近，剛好這個字一模一樣，且也都用天城體書寫為「धन्यवाद」。<a href='https://www.wikiwand.com/zh-tw/印地语'>Hindi 印地語</a>為印度共和國使用人口最多的語言，但若以這場戲的背景<b>大吉嶺</b>而言，尼泊爾語比印地語更通行，而且剛好兩語言又都是這個字，所以不管劇本這邊是要表達哪種語言，都與背景相符。</div>",/**/
-            "4"
+            "4",
+            "選"
           ],/* 這句本想獨立為一行並將天城體及羅馬轉寫分列，但考量到轉寫比較不具正式文字地位（不像我希望讓鶴佬話、客話的羅馬字地位提升），加上演員節奏是連貫的，所以還是挪到接在上句後面，只是放大處理。
           [
             "",
@@ -39,7 +40,8 @@ data = [
             "吉",
             "日仔仰般變，飯還係愛食，茶還係愛啉！",
             "Ngid'er ngíongbàn bǐen, pön han hě ǒi shìd, ca han hě ǒi lìm!",
-            ""
+            "",
+            "選"
           ],
         ]
       },
@@ -54,15 +56,16 @@ data = [
           ],
           [
             "石",
-            "社長……這係……？",
-            "しゃちょう... Lía hě...?",
+            "<span class='jp'><ruby><rb>社長</rb><rp>(</rp><rt>しゃちょう</rt><rp>)</rp></ruby></span>……這係……？",
+            "<span class='jp'>Shachō</span>... Lía hě...?",
             ""
           ],
           [
             "吉",
             "三十年个茶樹頭，拿分若師傅看。",
             "Sàmshìb ngien gǎi cashüteu, nà bùn ngia sìifü kǒn.",
-            ""
+            "",
+            "選"
           ],
         ]
       },
@@ -79,7 +82,8 @@ data = [
             "吉",
             "來到早，毋當來到堵堵好。<br>好時辰，堵堵好。",
             "Loi dǒ zó, m dòng loi dǒ dúdúhó.<br>Hó shishin, dúdúhó.",
-            ""
+            "",
+            "選"
           ],
           [
             "司儀",
@@ -92,6 +96,30 @@ data = [
       /* 9:27 做得做得 */
       /* 9:55 也不夠國民政府打戰 */
       /* ⭐ 11:55 茶虎茶壺 */
+      {
+        "t": "11:55",
+        "rows": [
+          [
+            "范",
+            "<div class='sidenote'>教育部詞典沒收「怡」字，只好先用廣韻「與之切」來推發音。</div><span class='jp'>吉さん</span>摎怡和聯手喔，就出口臺灣三<span class='TBD'>股</span>一<span class='TBD'>股</span>个茶。<br>桃竹苗个茶，全<span class='TBD'>就</span>分<span class='jp'><ruby><rb>社長</rb><rp>(</rp><rt>しゃちょう</rt><rp>)</rp></ruby></span>你收淨淨哩，這下大自家喔都喊你「茶虎」呢！",
+            "<div class='sidenote'>「吉」的鶴佬話、客話、日語的讀音，通通都不是 Ji，難道是華語音嗎？</div><span class='jp'><span class='noted'>Ji</span> San</span> làu <span class='TBD'>Rhì</span>fo lienshú ò, cïu chudkíeu Toivan sàm gú rhid gú gě ca.<br>To-Zhug-Miau gě ca, cion cïu bùn <span class='jp'>Shachō</span> ngi shìu cïangcïang lè, líahä täicidgà ò dü hèm ngi &quot;Ca Fú&quot; nè!",
+            ""
+          ],
+          [
+            "吉",
+            "麼个「茶虎」？係茶壺啦！<br>𠊎<span class='TBD'>＿</span>係，收茶、賣茶定定。",
+            "<div class='sidenote'>這是客語的「幾分之幾」的文法嗎？「股」我是用猜的，音似乎合。</div>Mágǎi &quot;Ca Fú&quot;, hě cafu là!<br>Ngai <span class='TBD'>sii</span> hě, shìu ca, mäi ca tïntïn.",
+            "",
+            "選"
+          ],
+          [
+            "范",
+            "<span class='jp'><ruby><rb>社長</rb><rp>(</rp><rt>しゃちょう</rt><rp>)</rp></ruby></span>係茶壺喔，𠊎就係茶杯 ě！",
+            "<span class='jp'>Shachō</span> hě cafu ò, ngai cïu hě cabùi ě!",
+            ""
+          ],
+        ]
+      }
       /* ⭐ 15:00 日語；法語 */
       /* 20:00 我穿裙；免啦。敢若囡仔 */
       /* ⭐ 25:00 賺兩塊花十塊；風神 */
