f6c6cb4abfb0f7a2f3fa0caceb02ba8b4e6d4fd3
GHSRobert Ciang <Robertus0617@gmail.com>
2022-05-08 00:59:27 +0800

    Update data.js
    「查有此人」客語版 YT 連結

--- data.js
+++ data.js
@@ -1105,7 +1105,7 @@ data = [
           [
             "",
             "",
-            "<iframe width='100%' height='315' src='https://www.youtube.com/embed/jVtytZZdorI' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
+            "<iframe width='100%' height='315' src='https://www.youtube.com/embed/jmd2xovN9xo' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
             ""
           ],
           [
