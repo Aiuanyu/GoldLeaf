57cea3bc8374ca0a4427224bf34055f3ba9a9893
GHSRobert Ciang <Robertus0617@gmail.com>
2022-05-10 17:33:43 +0800

    [愛灣語字卡] #4、#5

--- data_cards.js
+++ data_cards.js
@@ -5,16 +5,38 @@ cards = [
     "客語對樹的量詞為「頭」。",
     "1"
   ],
-    [
+  [
     "<span class='cardTextSmaller'>（～欉）</span>樹仔",
     "<span class='cardTextSmaller'>(~ tsâng) </span>tshiū-á",
     "鶴佬話對樹的量詞為「欉」。",
-    "2"
+    "2",
+    "90"
   ],
   [
-    "薑嫲",
-    "<span class='si'>gióngmǎ</span><br><span class='hl'>giòngma</span>",
-    "即「薑」，客語似乎要加「-嫲」這個詞尾，所以不單稱「薑」。<br><br>苗栗縣大湖、銅鑼、三義 3 鄉交界處山嶺地名「薑麻園/羌麻園」，其實語源就是「薑嫲園 gióngmǎ'iěn」（種薑的園地），完全只跟薑有關，與麻無關。（該地為四縣腔區）",
+    "化石",
+    "<span class='si'>fasag</span><br><span class='hl'>fǎshàg</span>",
+    "",
     "1"
   ],
+  [
+    "恐龍",
+    "<span class='si'>kiùngliǔng</span><br><span class='hl'><ruby><rb>kiúng</rb><rp>(</rp><rt>kiūng</rt><rp>)</rp></ruby>liung</span>",
+    "（標示變調：海陸腔 24 調前字一律變 33）",
+    "1",
+    "80"
+  ],
+  [
+    "薑嫲園",
+    "<span class='si'>gióngmǎ'iěn</span><br><span class='hl'>giòngmarhan</span>",
+    "「薑嫲」即「薑」，客語這個詞似乎必加「-嫲」詞尾，所以不單稱「薑」。<br><br>苗栗縣大湖、銅鑼、三義 3 鄉交界處山嶺地名「薑麻園/羌麻園」，其實語源就是「<b>薑嫲園 gióngmǎ'iěn</b>」（種薑的園地），完全只跟薑有關，與麻無關。（該地為四縣腔區）所以那裡真的就是盛產薑以及相關製品，餐廳簡餐還有薑黃飯 😆～",
+    "1",
+    "72"
+  ],
+  [
+    "油桐花",
+    "<span class='si'>iǔtǔngfá</span><br><span class='hl'>rhiutungfà</span>",
+    "",
+    "1",
+    "80"
+  ]
 ]
